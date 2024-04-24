1ae731cdeabb67bc8988c9ed540b590221c9dd35
Mike Baldwin <mikembaldwin@gmail.com>
2024-04-24 14:46:00 -0400

    Add RecordDrinkScreen

---
+++ DrinkTracker/RecordDrinkScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDrinkScreen.swift
+//  DrinkTracker
+//
+//  Created by Mike Baldwin on 4/24/24.
+//
+
+import SwiftUI
+
+struct RecordDrinkScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordDrinkScreen()
+}
