31effa8c84964485c55bbeae3649e0d9df9a491e
Mike Baldwin <mikembaldwin@gmail.com>
2024-04-29 09:13:11 -0400

    Revise bar chart formatting to show entire week

--- DrinkTracker/MainScreen.swift
+++ DrinkTracker/MainScreen.swift
@@ -131,18 +131,43 @@ struct MainScreen: View {
                     .font(.caption)
                     .foregroundStyle(.gray)
             }
-            .padding(EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 0))
+            .padding(
+                EdgeInsets(
+                    top: 6,
+                    leading: 0,
+                    bottom: 0,
+                    trailing: 0
+                )
+            )
             
+            let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
             Chart {
-                ForEach(dayLogs) { day in
-                    LineMark(
-                        x: .value("Date", day.date),
-                        y: .value("Drinks", day.totalDrinks)
+                ForEach(daysOfWeek, id: \.self) { day in
+                    // use the first(where:) method to find the corresponding
+                    // dayLog entry for each day of the week. We compare the
+                    // weekday component of the date property with the index of
+                    // the current day in the daysOfWeek array (plus 1 to match
+                    // the weekday numbering). If a matching entry is found, we
+                    // use its totalDrinks value; otherwise, we use 0 as the
+                    // default value.
+                    let totalDrinks = dayLogs.first(where: {
+                        Calendar.current.component(.weekday, from: $0.date) ==
+                        daysOfWeek.firstIndex(of: day)! + 1
+                    })?.totalDrinks ?? 0
+                    
+                    BarMark(
+                        x: .value("Day", day),
+                        y: .value("Drinks", totalDrinks)
                     )
                 }
             }
+            .chartXAxis {
+                AxisMarks(values: daysOfWeek) { _ in
+                    AxisValueLabel()
+                }
+            }
             .padding()
-            
         }
     }
     
