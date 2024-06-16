ed22c53a67909cd7d913a10ac69f3ac39db5a5e1
Mike Baldwin <mikembaldwin@gmail.com>
2024-06-16 14:15:50 -0400

    Add stub for RecordDrinksView

---
+++ DrinkTracker/RecordDrinks/RecordDrinksView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDrinksView.swift
+//  DrinkTracker
+//
+//  Created by Mike Baldwin on 6/16/24.
+//
+
+import SwiftUI
+
+struct RecordDrinksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordDrinksView()
+}
